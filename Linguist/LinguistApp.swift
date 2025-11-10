31766a721cc24e4b105050c4a4534d1bc02a2807
Michael <michaelchang90@gmail.com>
2025-11-10 12:00:16 -0500

    Initial Commit

---
+++ Linguist/LinguistApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinguistApp.swift
+//  Linguist
+//
+//  Created by Michael Chang on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LinguistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LinguistTests/LinguistTests.swift
@@ -0,0 +1,16 @@
+//
+//  LinguistTests.swift
+//  LinguistTests
+//
+//  Created by Michael Chang on 11/10/25.
+//
+
+import Testing
+
+struct LinguistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
